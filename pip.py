6f8718841c2ef2214c47b8b79ae73b106b2d43fb
Yvonne-Atieno <oyvonneatieno@gmail.com>
2023-04-21 20:29:42 +0300

    first commit

---
+++ pip.py
@@ -0,0 +1,26 @@
+#Declare an empty list
+
+#Declare a list with more than 5 items
+
+#Find the length of your list
+
+#Get the first item, the middle item and the last item of the list
+
+#Declare a list called mixed_data_types, put your(name, , height, marital status, address)age
+
+#Declare an empty list
+a=[]
+#Declare a list with more than 5
+b=['mango','orange','pineapple','apple','coconut','banana']
+print(b)
+#Find the lenght of your listixed
+print(len(b))
+
+#Get the first item, the middle item and the last item of the list
+b="mango"(middle)c("banana")
+print(b)
+#Declare a list called mixed_data_types, put your(name, , height, marital status, address)age
+mixed_data_types=("yvonne",34,"single","740 SIAYA",18)
+print(mixed_data_types)
+
+
